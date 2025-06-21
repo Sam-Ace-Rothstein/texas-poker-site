c215e549c5f1e720ea729381c66831b25f0f2de1
tom <tom@tom.tom>
2025-06-21 20:28:22 +0700

    fix: set Vite base to /texas-poker-site/

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/texas-poker-site/',    // ← add this
   plugins: [react()],
 })
